feeb8d524ef99f73ee1b27671d729110e0928893
Nafis185 <nafisfuad185@gmail.com>
2025-01-10 19:36:08 +0600

    Event

--- src/Pages/Home/Event/Event.jsx
+++ src/Pages/Home/Event/Event.jsx
@@ -2,9 +2,40 @@
 
 const Event = () => {
     return (
+      <div>
         <div>
-            <h1>this is event section </h1>
+          <h1 className="text-center text-5xl font-bold mb-5">
+            Event Sehedule{" "}
+          </h1>
+          <hr />
+          <div>
+            <div className="card m-10 rounded-md bg-base-300 px-4 py-8">
+              <h1 className="text-2xl font-bold">Opening Ceremony </h1>
+              <p>Day-01 09:00 AM -11.00 AM</p>
+            </div>
+            <div className="card m-10 rounded-md bg-base-300 px-4 py-8">
+              <h1 className="text-2xl font-bold">Opening Ceremony </h1>
+              <p>Day-01 09:00 AM -11.00 AM</p>
+            </div>
+            <div className="card m-10 rounded-md bg-base-300 px-4 py-8">
+              <h1 className="text-2xl font-bold">Opening Ceremony </h1>
+              <p>Day-01 09:00 AM -11.00 AM</p>
+            </div>
+            <div className="card m-10 rounded-md bg-base-300 px-4 py-8">
+              <h1 className="text-2xl font-bold">Opening Ceremony </h1>
+              <p>Day-01 09:00 AM -11.00 AM</p>
+            </div>
+            <div className="card m-10 rounded-md bg-base-300 px-4 py-8">
+              <h1 className="text-2xl font-bold">Opening Ceremony </h1>
+              <p>Day-01 09:00 AM -11.00 AM</p>
+            </div>
+            <div className="card m-10 rounded-md bg-base-300 px-4 py-8">
+              <h1 className="text-2xl font-bold">Opening Ceremony </h1>
+              <p>Day-01 09:00 AM -11.00 AM</p>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
